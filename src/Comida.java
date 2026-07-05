0c9e38c136bdca9223627517bdbfb6abc7ebc56f
JavoGonza10 <blasjgj10@gmail.com>
2026-07-05 18:35:32 +0200

    Add: clases Bebida y Comida heredando de Producto

---
+++ src/Comida.java
@@ -0,0 +1,22 @@
+public class Comida extends Producto{
+
+    private boolean esCaliente;
+
+
+    public Comida(String nombre, String categoria, double precio, boolean esCaliente){
+        super(nombre,categoria,precio);
+        this.esCaliente =esCaliente;
+
+    }
+
+    public boolean isEsCaliente() {
+        return esCaliente;
+    }
+
+    public void setEsCaliente(boolean esCaliente) {
+        this.esCaliente = esCaliente;
+    }
+}
+
+
+
