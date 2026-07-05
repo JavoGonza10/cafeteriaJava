d606d08811693c788be9a53644881ce87fbe7f47
JavoGonza10 <blasjgj10@gmail.com>
2026-07-05 19:50:43 +0200

    Add: metodo agregarProducto en Ticket

--- src/Ticket.java
+++ src/Ticket.java
@@ -11,4 +11,14 @@ public class Ticket {
         this.productos = new Producto[10];
         this.camarero = camarero;
     }
+
+    public void agregarProducto(Producto producto) {
+        if (contadorProductos < productos.length) {
+            productos[contadorProductos] = producto;
+            contadorProductos++;
+        } else {
+            System.out.println("No se pueden añadir más productos, el ticket está lleno.");
+        }
+    }
+
 }
